cb573505cc97ac5f044fcdf5219a3f2003754799
Stepan <stepan1006@yandex.ru>
2019-07-31 12:32:33 +0200

    Initial Commit

---
+++ My Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Places
+//
+//  Created by Степан on 31.07.2019.
+//  Copyright © 2019 Stepan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
