320b82c15d7ab69605a9b10bd9f6dda3531b8b3c
Stepan <stepan1006@yandex.ru>
2019-08-03 16:51:05 +0200

    Custom Table View Cell

---
+++ My Places/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  My Places
+//
+//  Created by Степан on 03.08.2019.
+//  Copyright © 2019 Stepan. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
